f174322f7deecf10b83c317aa27e15186f2e2556
Aisha Husseini <ayasha.husseini@gmail.com>
2023-01-27 16:55:57 -0500

    added database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBvomVjtxtZwDNrHPWV_b6n4Rq9htvqRsA",
@@ -7,9 +9,11 @@ const firebaseConfig = {
   storageBucket: "swampy-sentiments-5e0b6.appspot.com",
   messagingSenderId: "56045351157",
   appId: "1:56045351157:web:3dce708a100fd0f85455bc",
-  measurementId: "G-7K7TCC469L"
+  measurementId: "G-7K7TCC469L",
+  databaseURL: "https://swampy-sentiments-5e0b6-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
