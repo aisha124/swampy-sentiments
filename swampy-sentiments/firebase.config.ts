a71a2c7a42932de8ce3e1464a72c3cfd0f1e10c5
Aisha Husseini <ayasha.husseini@gmail.com>
2023-01-28 14:49:07 -0500

    troubleshooting

---
+++ swampy-sentiments/firebase.config.ts
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBvomVjtxtZwDNrHPWV_b6n4Rq9htvqRsA",
+    authDomain: "swampy-sentiments-5e0b6.firebaseapp.com",
+    databaseURL: "https://swampy-sentiments-5e0b6-default-rtdb.firebaseio.com",
+    projectId: "swampy-sentiments-5e0b6",
+    storageBucket: "swampy-sentiments-5e0b6.appspot.com",
+    messagingSenderId: "56045351157",
+    appId: "1:56045351157:web:3dce708a100fd0f85455bc",
+    measurementId: "G-7K7TCC469L"
+  };
+
+  firebase.initializeApp(firebaseConfig);
