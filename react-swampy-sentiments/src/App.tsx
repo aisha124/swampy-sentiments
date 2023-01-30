bc7b9c90b06957043daf133e8fc393296d791c15
Aisha Husseini <ayasha.husseini@gmail.com>
2023-01-30 11:46:19 -0500

    Swampy Sentiments title

--- react-swampy-sentiments/src/App.tsx
+++ react-swampy-sentiments/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className='Swampy'>Swampy Sentiments</h1>
         <div className='currentDate'>
           <CurrentDate/>
         </div>
